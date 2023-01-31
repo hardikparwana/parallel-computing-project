b861bd423f04afdcd97bc0b7723aadbee042bc58
Hardik Parwana <hardikparwana@github.com>
2023-01-31 03:46:50 -0500

    double version + removed debug statements

--- code/main.cu
+++ code/main.cu
@@ -8,8 +8,8 @@
 using namespace std;
 
 #define SIZE 5
-#define NUM 1000
-#define THREADS 100
+#define NUM 10000
+#define THREADS 200
 
 __global__ void cuda_mean( float* in_vectors, int n, int N, float *out_vector ){
 	/*
@@ -19,9 +19,7 @@ __global__ void cuda_mean( float* in_vectors, int n, int N, float *out_vector ){
 
     __shared__ float temp_sum[THREADS * SIZE];// 1024 by default //sum done by each thread in shared memory //
 	__shared__ float temp_sum2[THREADS * SIZE];
-    // printf("%d, %d, %f %f \n", threadIdx.x, threadIdx.x + n, in_vectors[threadIdx.x], in_vectors[threadIdx.x + n]);
-	// each thread does computation for all the elements
-
+ 
 	for (int j=0; j<N; j++){
 		temp_sum[threadIdx.x + THREADS*j] = 0;
 		temp_sum2[threadIdx.x + THREADS*j] = 0;
@@ -36,9 +34,7 @@ __global__ void cuda_mean( float* in_vectors, int n, int N, float *out_vector ){
 					temp_sum[ j*THREADS + threadIdx.x ] = temp_sum[ j*THREADS + threadIdx.x ] + in_vectors[ threadIdx.x + i*THREADS + j*n  ]; // threadIdx.x + i*1024 is vector number and we need j-th element of it
 				}
 		}
-	}// this will ensure all min(n,1024 active threads)
-    // printf("id: %d, %f, %f ", threadIdx.x, temp_sum[threadIdx.x], temp_sum[threadIdx.x+1024]);
-	// now we have max 1024 vectors only. but last few columns empty possibly
+	}
 	__syncthreads(); // do this to have left 1024 max elements that now need to be summed
 
 	int elements_per_thread = 5; // no reason for particularly choosing 5 but we will be now summing in groups of 5. 
@@ -51,7 +47,6 @@ __global__ void cuda_mean( float* in_vectors, int n, int N, float *out_vector ){
 		active_threads = n;
 	int new_active_threads = 1 + (active_threads-1) / elements_per_thread; // active threads after summed in groups of 5
 	
-	// temp_sum2[threadIdx.x] = 0;
 	__syncthreads();
 
 	while (active_threads > 1){//elements_per_thread){
@@ -112,23 +107,18 @@ __global__ void cuda_covariance( float* in_vectors, int n, int N, float* mu, flo
 		}
         
 		int num_elements_per_thread = (n + THREADS - 1) / THREADS;
-        // if (threadIdx.x == 0)
-        //     printf("ele per thread: %d \n", num_elements_per_thread);
-	
+      
 		// subtract mu from particles first
 		__syncthreads();
 			for (int i=0; i<num_elements_per_thread; i++){			
                 for (int j=0; j<N; j++){
 				if ((threadIdx.x + i*THREADS) < n){  // vector number = round robbin fashion. (not consecutive vectors)
-                // printf("id: %d, element no: %d \n", threadIdx.x, j*n + threadIdx.x);
 					in_vectors[ j*n + threadIdx.x + i*THREADS ] = in_vectors[ j*n + threadIdx.x + i*THREADS ] - mu[j]/n;
-                    // if (threadIdx.x == 1000)
-                    //     printf("id: %d, element no: %d, element: %f \n", threadIdx.x, j*n + threadIdx.x + i*1024, in_vectors[ j*n + threadIdx.x + i*1024 ]);
 				}
 			}
 		}
 		__syncthreads();
-		//
+		
 		// compute N^2 matrices for each vector. each thread takes num_elements_per_thread
 		int vector_start_number = threadIdx.x * num_elements_per_thread; // start vector number
 
@@ -138,41 +128,16 @@ __global__ void cuda_covariance( float* in_vectors, int n, int N, float* mu, flo
 
                 for (int i=0; i<num_elements_per_thread; i++){ // vector number
                     if ((threadIdx.x + i*THREADS) < n){                
-                        // j,k element of N^ matrix to be computed here					
-                            // in_matrix[ (threadIdx.x + i*1024)*N*N + j*N + k ] = in_matrix[ (threadIdx.x + i*1024)*N*N + j*N + k ] + in_vectors[ threadIdx.x + i*1024 + j*n ] * in_vectors[ threadIdx.x + i*1024 + k*n ];
                             int element_number = j*N + k;
                             int Idx_mod = threadIdx.x + i*THREADS;
                             if (Idx_mod >= THREADS)
                                 Idx_mod = Idx_mod % THREADS;
-                            // in_matrix[ (threadIdx.x + i*1024) + element_number * 1024 ] = in_matrix[ (threadIdx.x + i*1024) + element_number * 1024 ] + in_vectors[ threadIdx.x + i*1024 + j*n ] * in_vectors[ threadIdx.x + i*1024 + k*n ];
                             temp_sum[ (Idx_mod) + element_number * THREADS ] = temp_sum[ (Idx_mod) + element_number * THREADS ] + in_vectors[ threadIdx.x + i*THREADS + j*n ] * in_vectors[ threadIdx.x + i*THREADS + k*n ];
-                            // 4 + 4 : 8 elements
-                            // if (threadIdx.x == 0)
-                            //     printf(" id: %d, element number: %d , element: %f, vector j:%d, vector k: %d \n", threadIdx.x, (threadIdx.x + i*1024) + element_number * 1024, in_matrix[ (threadIdx.x + i*1024) + element_number * 1024 ], threadIdx.x + i*1024 + j*n, threadIdx.x + i*1024 + k*n);
                         } // each matrix takes N*N elements
                     }				
 			}
 		}// this will ensure all min(n,1024 active threads)
 		__syncthreads(); // do this to have left 1024 max elements that now need to be summed
-
-		//collapse first to 1024 matrices now (elements), each vector N*N elements
-        // int bound  = 1024;
-        // if (n <= 1024)
-        //     bound = n;
-		// int startIdx = threadIdx.x;
-		// for (int j=0; j<(N*N); j++){
-		// 		for (int i=0; i<num_elements_per_thread; i++){
-		// 			if ((threadIdx.x + i*1024) < bound){ //(not consecutive vectors)
-		// 				temp_sum[ threadIdx.x + j*1024 ] = temp_sum[ threadIdx.x + j*1024 ] + in_matrix[ threadIdx.x + i*1024 + j*1024 ];
-        //                 if (threadIdx.x==0)
-        //                     printf("collapsed id: %d, element number: %d, element: %f, matrix col: %d \n", threadIdx.x, j*1024 + threadIdx.x, temp_sum[j*1024 + threadIdx.x], threadIdx.x + i*1024 + j*1024);
-		// 			}
-		// 	}
-		// }// this will ensure all min(n,1024 active threads)
-
-		// __syncthreads(); // do this to have left 1024 max elements that now need to be summed
-
-		// now add all matrices. treat them same as vectors with N^2 elements
 
 		int elements_per_thread = 5; // no reason for particularly choosing 5 but we will be now summing in groups of 5. 
 		// this reduces the size of array by 5 every time and the procedure is repetated until less than 5 threads remain
@@ -184,8 +149,8 @@ __global__ void cuda_covariance( float* in_vectors, int n, int N, float* mu, flo
 			active_threads = n;
 		int new_active_threads = 1 + (active_threads-1) / elements_per_thread; // active threads after summed in groups of 5
 		
-		temp_sum2[threadIdx.x] = 0;
-		__syncthreads();
+		// temp_sum2[threadIdx.x] = 0;
+		// __syncthreads();
 
 		while (active_threads > 1){//elements_per_thread){
 		int startIdx = threadIdx.x * elements_per_thread; // start vector number. now take 5 consecutive vectors
@@ -246,10 +211,7 @@ int main(){
     
     float *d_mean;
     float *d_cov;
-    // cout << "hello1" << endl;
-    // std::random_device rd{};
-    // std::mt19937 gen{rd()};
-    // std::normal_distribution<> d{init_mean,init_std};
+ 
     for (int i=0; i<n; i++){
         for (int j=0; j<N; j++){
             vectors[i + j*n] = i + sin(j*i);
@@ -264,11 +226,7 @@ int main(){
     }
     // vectors[0] = 3; vectors[1] = 6; vectors[2] = 4;
     // vectors[3] = 7; vectors[4] = 12; vectors[5] = -9;
-    // for (int i=0; i<N*n; i++){
-    //     cout << "vectors " << i << "\t" << vectors[i] << endl;
-    // }
-    // cout << "hello2" << endl;
-    // X^T A X + B
+  
    
     ///////////////// Serial Computation ////////////////////////////
 
@@ -278,19 +236,19 @@ int main(){
     float cov[N*N];
     float cov_out[N*N];
 
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     for (int i=0; i<N; i++){
         mean[i] = 0;
         for (int j=0; j<N; j++){
             cov[i*N+j] = 0;
         }
     }
+
     for (int i=0; i<n; i++){
         for (int j=0; j<N; j++){
-            // cout << "element number: " << (i+n*j) << endl;
             mean[j] = mean[j] + vectors[ i + n*j ];
         }
     }
-    cout << "hello3" << "\t" << mean[0]/n << "\t" << mean[1]/n << "\t" << mean[2]/n << "\t" << mean[3]/n << endl;
     
     for (int i=0; i<n; i++){
         for (int j=0; j<N; j++){
@@ -299,36 +257,42 @@ int main(){
             }
         }
     }
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Time(milliseconds) elapsed to perform mean, cov: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()/1000.0 << std::endl;
+    cout << "hello3" << "\t" << mean[0]/n << "\t" << mean[1]/n << "\t" << mean[2]/n << "\t" << mean[3]/n << endl;
     cout << "hello4" << "\t" << cov[0]/(n-1) << "\t" << cov[1]/(n-1) << "\t" << cov[2]/(n-1) << "\t" << cov[3]/(n-1) << endl;
     //////////////// Parallel Computation ////////////////////////
     int num_threads = n;
     int num_blocks = 1;
     int max_threads_per_block = THREADS;
-    // int num_blocks = ( num_threads + max_threads_per_block - 1 ) / max_threads_per_block;
 
-    // Initialize data to device
-    // cout << "hello y0" << endl;
-    // int size = N*n;
-	
-    // cout << "hello y1" << endl;
+  
+    
     if (cudaMalloc(&d_mean, N*sizeof(float)) != cudaSuccess){
 		cout << "Could not allocate d_mean" << endl;
 	}
-    // cout << "hello y2" << endl;
     if (cudaMalloc(&d_cov, N*N*sizeof(float)) != cudaSuccess){
 		cout << "Could not allocate d_cov" << endl;
 	}
-    // cout << "hello y" << endl;
     if (cudaMemcpy( d_vectors, vectors, N*n*sizeof(float), cudaMemcpyHostToDevice ) !=cudaSuccess){
 		cout << "Could not copy vectors to d_vectors" << endl;
 	}
 	cout << "CUDA kernel call for matrix operations: num blocks: " << num_blocks << " threads " << max_threads_per_block << std::endl;	
 
+    cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start);
+
     cuda_mean<<< num_blocks, THREADS >>>( d_vectors, n, N, d_mean );
     cuda_covariance<<< num_blocks, THREADS >>>( d_vectors, n, N, d_mean, d_cov );
 
-    cout << "hello5" << endl;
-    // copy output to input matrix
+    cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+    float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, start, stop);
+    cout << "Time(milliseconds) elapsed to perform 10 iterations CUDA: " << milliseconds << endl;
+
     cudaMemcpy(mean_out, d_mean, N*sizeof(float), cudaMemcpyDeviceToHost  );	
     cudaMemcpy(cov_out, d_cov, N*N*sizeof(float), cudaMemcpyDeviceToHost  );	
 
--- code/main_double.cu
+++ code/main_double.cu
@@ -9,7 +9,7 @@ using namespace std;
 
 #define SIZE 2
 #define NUM 1500
-#define THREADS 100
+#define THREADS 500
 
 __global__ void cuda_mean( double* in_vectors, int n, int N, double *out_vector ){
 	/*
@@ -19,9 +19,7 @@ __global__ void cuda_mean( double* in_vectors, int n, int N, double *out_vector 
 
     __shared__ double temp_sum[THREADS * SIZE];// 1024 by default //sum done by each thread in shared memory //
 	__shared__ double temp_sum2[THREADS * SIZE];
-    // printf("%d, %d, %f %f \n", threadIdx.x, threadIdx.x + n, in_vectors[threadIdx.x], in_vectors[threadIdx.x + n]);
-	// each thread does computation for all the elements
-
+   
 	for (int j=0; j<N; j++){
 		temp_sum[threadIdx.x + THREADS*j] = 0;
 		temp_sum2[threadIdx.x + THREADS*j] = 0;
@@ -30,15 +28,14 @@ __global__ void cuda_mean( double* in_vectors, int n, int N, double *out_vector 
 	int num_elements_per_thread = (n + THREADS - 1) / THREADS;
 
 	// first collapse to 1024 elements only
+    __syncthreads();
 	for (int j=0; j<N; j++){
 		for (int i=0; i<num_elements_per_thread; i++){
 				if ( (threadIdx.x + i*THREADS) < n ){
 					temp_sum[ j*THREADS + threadIdx.x ] = temp_sum[ j*THREADS + threadIdx.x ] + in_vectors[ threadIdx.x + i*THREADS + j*n  ]; // threadIdx.x + i*1024 is vector number and we need j-th element of it
 				}
 		}
-	}// this will ensure all min(n,1024 active threads)
-    // printf("id: %d, %f, %f ", threadIdx.x, temp_sum[threadIdx.x], temp_sum[threadIdx.x+1024]);
-	// now we have max 1024 vectors only. but last few columns empty possibly
+	}
 	__syncthreads(); // do this to have left 1024 max elements that now need to be summed
 
 	int elements_per_thread = 5; // no reason for particularly choosing 5 but we will be now summing in groups of 5. 
@@ -51,8 +48,7 @@ __global__ void cuda_mean( double* in_vectors, int n, int N, double *out_vector 
 		active_threads = n;
 	int new_active_threads = 1 + (active_threads-1) / elements_per_thread; // active threads after summed in groups of 5
 	
-	// temp_sum2[threadIdx.x] = 0;
-	__syncthreads();
+	// __syncthreads();
 
 	while (active_threads > 1){//elements_per_thread){
 		int startIdx = threadIdx.x * elements_per_thread; // start vector number. now take 5 consecutive vectors
@@ -112,23 +108,18 @@ __global__ void cuda_covariance( double* in_vectors, int n, int N, double* mu, d
 		}
         
 		int num_elements_per_thread = (n + THREADS - 1) / THREADS;
-        // if (threadIdx.x == 0)
-        //     printf("ele per thread: %d \n", num_elements_per_thread);
-	
+       
 		// subtract mu from particles first
 		__syncthreads();
 			for (int i=0; i<num_elements_per_thread; i++){			
                 for (int j=0; j<N; j++){
 				if ((threadIdx.x + i*THREADS) < n){  // vector number = round robbin fashion. (not consecutive vectors)
-                // printf("id: %d, element no: %d \n", threadIdx.x, j*n + threadIdx.x);
 					in_vectors[ j*n + threadIdx.x + i*THREADS ] = in_vectors[ j*n + threadIdx.x + i*THREADS ] - mu[j]/n;
-                    // if (threadIdx.x == 1000)
-                    //     printf("id: %d, element no: %d, element: %f \n", threadIdx.x, j*n + threadIdx.x + i*1024, in_vectors[ j*n + threadIdx.x + i*1024 ]);
 				}
 			}
 		}
 		__syncthreads();
-		//
+		
 		// compute N^2 matrices for each vector. each thread takes num_elements_per_thread
 		int vector_start_number = threadIdx.x * num_elements_per_thread; // start vector number
 
@@ -138,41 +129,16 @@ __global__ void cuda_covariance( double* in_vectors, int n, int N, double* mu, d
 
                 for (int i=0; i<num_elements_per_thread; i++){ // vector number
                     if ((threadIdx.x + i*THREADS) < n){                
-                        // j,k element of N^ matrix to be computed here					
-                            // in_matrix[ (threadIdx.x + i*1024)*N*N + j*N + k ] = in_matrix[ (threadIdx.x + i*1024)*N*N + j*N + k ] + in_vectors[ threadIdx.x + i*1024 + j*n ] * in_vectors[ threadIdx.x + i*1024 + k*n ];
                             int element_number = j*N + k;
                             int Idx_mod = threadIdx.x + i*THREADS;
                             if (Idx_mod >= THREADS)
                                 Idx_mod = Idx_mod % THREADS;
-                            // in_matrix[ (threadIdx.x + i*1024) + element_number * 1024 ] = in_matrix[ (threadIdx.x + i*1024) + element_number * 1024 ] + in_vectors[ threadIdx.x + i*1024 + j*n ] * in_vectors[ threadIdx.x + i*1024 + k*n ];
                             temp_sum[ (Idx_mod) + element_number * THREADS ] = temp_sum[ (Idx_mod) + element_number * THREADS ] + in_vectors[ threadIdx.x + i*THREADS + j*n ] * in_vectors[ threadIdx.x + i*THREADS + k*n ];
-                            // 4 + 4 : 8 elements
-                            // if (threadIdx.x == 0)
-                            //     printf(" id: %d, element number: %d , element: %f, vector j:%d, vector k: %d \n", threadIdx.x, (threadIdx.x + i*1024) + element_number * 1024, in_matrix[ (threadIdx.x + i*1024) + element_number * 1024 ], threadIdx.x + i*1024 + j*n, threadIdx.x + i*1024 + k*n);
                         } // each matrix takes N*N elements
                     }				
 			}
 		}// this will ensure all min(n,1024 active threads)
 		__syncthreads(); // do this to have left 1024 max elements that now need to be summed
-
-		//collapse first to 1024 matrices now (elements), each vector N*N elements
-        // int bound  = 1024;
-        // if (n <= 1024)
-        //     bound = n;
-		// int startIdx = threadIdx.x;
-		// for (int j=0; j<(N*N); j++){
-		// 		for (int i=0; i<num_elements_per_thread; i++){
-		// 			if ((threadIdx.x + i*1024) < bound){ //(not consecutive vectors)
-		// 				temp_sum[ threadIdx.x + j*1024 ] = temp_sum[ threadIdx.x + j*1024 ] + in_matrix[ threadIdx.x + i*1024 + j*1024 ];
-        //                 if (threadIdx.x==0)
-        //                     printf("collapsed id: %d, element number: %d, element: %f, matrix col: %d \n", threadIdx.x, j*1024 + threadIdx.x, temp_sum[j*1024 + threadIdx.x], threadIdx.x + i*1024 + j*1024);
-		// 			}
-		// 	}
-		// }// this will ensure all min(n,1024 active threads)
-
-		// __syncthreads(); // do this to have left 1024 max elements that now need to be summed
-
-		// now add all matrices. treat them same as vectors with N^2 elements
 
 		int elements_per_thread = 5; // no reason for particularly choosing 5 but we will be now summing in groups of 5. 
 		// this reduces the size of array by 5 every time and the procedure is repetated until less than 5 threads remain
@@ -205,7 +171,6 @@ __global__ void cuda_covariance( double* in_vectors, int n, int N, double* mu, d
 		if (startIdx < active_threads){
 			for (int j=0; j<N*N; j++){
 					temp_sum[j*THREADS + threadIdx.x] = temp_sum2[j*THREADS + startIdx];
-                    // printf("copied id: %d, element number: %d, element: %f \n", threadIdx.x, j*1024 + threadIdx.x, temp_sum[j*1024 + threadIdx.x]);
 			}
 		}
 
@@ -234,7 +199,7 @@ int main(){
     
     double init_mean = 0;
     double init_std = 1.0;
-    // cout << "hello" << endl;
+   
     // initialize vectors
     int size = N*n * sizeof(double);
     double *vectors = (double *)malloc(size);
@@ -246,7 +211,7 @@ int main(){
     
     double *d_mean;
     double *d_cov;
-    // cout << "hello1" << endl;
+
     // std::random_device rd{};
     // std::mt19937 gen{rd()};
     // std::normal_distribution<> d{init_mean,init_std};
@@ -264,11 +229,6 @@ int main(){
     }
     // vectors[0] = 3; vectors[1] = 6; vectors[2] = 4;
     // vectors[3] = 7; vectors[4] = 12; vectors[5] = -9;
-    // for (int i=0; i<N*n; i++){
-    //     cout << "vectors " << i << "\t" << vectors[i] << endl;
-    // }
-    // cout << "hello2" << endl;
-    // X^T A X + B
    
     ///////////////// Serial Computation ////////////////////////////
 
@@ -278,6 +238,7 @@ int main(){
     double cov[N*N];
     double cov_out[N*N];
 
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     for (int i=0; i<N; i++){
         mean[i] = 0;
         for (int j=0; j<N; j++){
@@ -290,7 +251,6 @@ int main(){
             mean[j] = mean[j] + vectors[ i + n*j ];
         }
     }
-    cout << "hello3" << "\t" << mean[0]/n << "\t" << mean[1]/n << "\t" << mean[2]/n << "\t" << mean[3]/n << endl;
     
     for (int i=0; i<n; i++){
         for (int j=0; j<N; j++){
@@ -299,6 +259,9 @@ int main(){
             }
         }
     }
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Time(milliseconds) elapsed to perform mean, cov: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()/1000.0 << std::endl;
+    cout << "hello3" << "\t" << mean[0]/n << "\t" << mean[1]/n << "\t" << mean[2]/n << "\t" << mean[3]/n << endl;
     cout << "hello4" << "\t" << cov[0]/(n-1) << "\t" << cov[1]/(n-1) << "\t" << cov[2]/(n-1) << "\t" << cov[3]/(n-1) << endl;
     //////////////// Parallel Computation ////////////////////////
     int num_threads = n;
@@ -306,28 +269,34 @@ int main(){
     int max_threads_per_block = THREADS;
     // int num_blocks = ( num_threads + max_threads_per_block - 1 ) / max_threads_per_block;
 
-    // Initialize data to device
-    // cout << "hello y0" << endl;
-    // int size = N*n;
-	
-    // cout << "hello y1" << endl;
+ 
     if (cudaMalloc(&d_mean, N*sizeof(double)) != cudaSuccess){
 		cout << "Could not allocate d_mean" << endl;
 	}
-    // cout << "hello y2" << endl;
+   
     if (cudaMalloc(&d_cov, N*N*sizeof(double)) != cudaSuccess){
 		cout << "Could not allocate d_cov" << endl;
 	}
-    // cout << "hello y" << endl;
+   
     if (cudaMemcpy( d_vectors, vectors, N*n*sizeof(double), cudaMemcpyHostToDevice ) !=cudaSuccess){
 		cout << "Could not copy vectors to d_vectors" << endl;
 	}
 	cout << "CUDA kernel call for matrix operations: num blocks: " << num_blocks << " threads " << max_threads_per_block << std::endl;	
 
+    cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start);
+
     cuda_mean<<< num_blocks, THREADS >>>( d_vectors, n, N, d_mean );
     cuda_covariance<<< num_blocks, THREADS >>>( d_vectors, n, N, d_mean, d_cov );
 
-    cout << "hello5" << endl;
+    cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+    float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, start, stop);
+    cout << "Time(milliseconds) elapsed to perform 10 iterations CUDA: " << milliseconds << endl;
+
     // copy output to input matrix
     cudaMemcpy(mean_out, d_mean, N*sizeof(double), cudaMemcpyDeviceToHost  );	
     cudaMemcpy(cov_out, d_cov, N*N*sizeof(double), cudaMemcpyDeviceToHost  );	
